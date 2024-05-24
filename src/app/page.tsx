ec9b937fbdf4d163dd91dbadedb32c94e00952f8
cSahu14 <sahuchidananda1999@gmail.com>
2024-05-24 12:39:57 +0530

    call 3 apis for guess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,105 @@
-import Image from "next/image";
+'use client';
+import Loading from '@/components/Loading';
+import React, { useRef, useState } from 'react';
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(null);
+  const [gender, setGender] = useState('');
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  // const userInput = useRef<HTMLInputElement | null>(null);
+  const handleSubmitName = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    // fetch 3 apis
+    try {
+      const promises = [
+        fetch(`https://api.agify.io?name=${username}`, {
+          cache: 'no-store',
+        })
+          .then((res) => {
+            res.json();
+          })
+          .catch((err) => console.log('err', err)),
+        fetch(`https://api.genderize.io?name=${username}`, {
+          cache: 'no-store',
+        })
+          .then((res) => res.json())
+          .catch((err) => console.log('err')),
+        fetch(`https://api.nationalize.io?name=${username}`, {
+          cache: 'no-store',
+        })
+          .then((res) => res.json())
+          .catch((err) => console.log('err')),
+      ];
+
+      // Wait for all the promises to resolve and get the data
+      (await Promise.all(promises)).map((p) => {
+        // Set error
+        if (!p.age || !p.gender || !p.country[0]) {
+          setError('Error while fetching data.');
+        }
+        if (p.age) {
+          setAge(p?.age);
+        } else if (p.gender) {
+          setGender(p?.gender);
+        } else if (p.country) {
+          setCountry(p.country[0].country_id);
+        }
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log('error', error);
+      // Set error
+      setLoading(false);
+      setError('Error while fetching data.');
+      throw new Error('Error while fetching data.');
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className='h-screen flex items-center flex-col'>
+      {/* form submit */}
+      <form onSubmit={handleSubmitName} className='mt-8 flex'>
+        <div className='mr-2'>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='username'
+            type='text'
+            placeholder='Username'
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            Submit
+          </button>
+        </div>
+      </form>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {loading ? (
+        // Use skeleton for loading state
+        <Loading />
+      ) : // Check if age, gender and country is not empty
+      age && gender && country ? (
+        <div className='relative flex items-center flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96'>
+          <div className='p-6'>
+            <h5 className='block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
+              Age - {age}
+            </h5>
+            <h5 className='block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
+              Gender - {gender}
+            </h5>
+            <h5 className='block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
+              Country - {country}
+            </h5>
+          </div>
+        </div>
+      ) : (
+        <div>{error}</div>
+      )}
+    </section>
   );
 }
